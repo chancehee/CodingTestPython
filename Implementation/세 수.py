8f8ee7f0f433d6ec623fe90ae9ed3c7c22a09bf2
chan9512 <dojsfffff@naver.com>
2021-08-08 00:44:55 +0900

    구현: 세 수

---
+++ Implementation/세 수.py
@@ -0,0 +1,25 @@
+'''
+구현: <세 수>
+문제
+세 정수 A, B, C가 주어진다. 이때, 두 번째로 큰 정수를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 세 정수 A, B, C가 공백으로 구분되어 주어진다. (1 ≤ A, B, C ≤ 100)
+
+출력
+두 번째로 큰 정수를 출력한다.
+'''
+
+A,B,C = map(int,input().split())
+list = [A,B,C]
+
+list.remove(min(list))
+
+if list[0]<list[1]:
+    print(list[0])
+else:
+    print(list[1])
+
+
+
+
