075fe4e2dc1275296038cb6f298bfcb964cfef58
chan <dojsfffff@naver.com>
2022-02-04 21:18:25 +0900

    브루트포스: 영화감독 숌

---
+++ BruteForce/영화감독 숌.py
@@ -0,0 +1,23 @@
+'''
+브루트 포스: <영화감독 숌>
+'''
+
+n = int(input())
+t = 666
+
+result = 1
+
+while True:
+    if result == n:
+        print(t)
+        break
+    t += 1
+    if '666' in str(t):
+        result += 1
+
+
+
+
+
+
+
